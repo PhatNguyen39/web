56b931a1c6ace3dccbc15013e0c0d059b973fbf2
Phat Nguyen <pnguyen340@gatech.edu>
2023-10-05 14:30:36 -0700

    initial commit

---
+++ Python_streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def main():
+    st.title("Simple Python Website with Streamlit")
+    st.header("Welcome to My Website")
+    st.write("This is a simple example of a website built with Streamlit.")
+
+    # Add more content and interactive elements as needed
+
+
+if __name__ == "__main__":
+    main()
